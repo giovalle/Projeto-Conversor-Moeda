6c0193b783fb409c81f2bffba408cc84bfe4898b
giovalle <asd.giovani2@gmail.com>
2021-04-06 19:50:21 -0300

    projeto Giovani Lovatto Allegretti conversor do real

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
+//Conversor de moedas do real
+//Giovani Lovatto Allegretti
+//import dos programas externos
 import React, { useState, useEffect } from 'react'
 import './app.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+//import dos componentes do bootstrap
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+//import icones react
+import { BiMoney } from "react-icons/bi";
 
 
 
 function App() {
-
+    //Definição de variaveis
     const [moeda, setMoeda] = useState('USD')
     const [codigo, setCodigo] = useState('')
     const [valor, setValor] = useState('')
     const [valorconvert, setValorconvert] = useState('')
 
-useEffect(()=>{
-    if (!valor||!codigo){
-        return
-    }
-console.log(codigo[0].high)
-console.log(valor/codigo[0].high)
-setValorconvert((valor/codigo[0].high).toFixed(2))
-},[codigo,valor])
+    //Função para fazer a conversão
+    useEffect(() => {
+        if (!valor || !codigo) {
+            return
+        }
+        console.log(codigo[0].high)
+        console.log(valor / codigo[0].high)         // até aqui é mostrado apenas no console para desbug
+        setValorconvert((valor / codigo[0].high).toFixed(2))      //parte mais importante do código aonde faz a conversão
+    }, [codigo, valor])
 
+    //função async que carrega a API
     async function obterMoeda(obtmoeda) {
-        const codigo = ('')
         let url = `https://economia.awesomeapi.com.br/json/${moeda}`
         console.log(url)
         await fetch(url)
@@ -36,49 +40,33 @@ setValorconvert((valor/codigo[0].high).toFixed(2))
             .then(data => {
                 setCodigo(data)
                 console.log('Dados Carregados com sucesso')
-                console.log(codigo)
-    
-            })}  
-    
+
+            })
+    }
+
     return (
+        //apartir daqui é o front-end aonde será trabalhada a formatação com a organização do projeto.
         <>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#inicio">Conversor de Moedas</Navbar.Brand>
+                <Navbar.Brand href="#inicio"><BiMoney />Conversor de Real para outras moedas</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#inicio">Início</Nav.Link>
-                    <Nav.Link href="#contato">Contato</Nav.Link>
+                    <Nav.Link href="#fatecitu" className="fatecitu">Fatec Itu</Nav.Link>
                 </Nav>
             </Navbar>
 
             <Jumbotron>
-                <h1>Conversor de Moeda</h1>
+                <h1><BiMoney size="60px" bg="success" />Conversor de Moeda</h1>
 
 
             </Jumbotron>
             <Card className="cartao" bg="dark">
+                <Card.Header className="cabecalho" style={{background: "#b3f37e"}} >Conversor</Card.Header>
                 <form className="quadro">
-                    <label className="fonteform"> Qual moeda gostaria de converter:</label>
-                    <select>
-                        <option value="BRL">Real</option>
-                        <option value="USD">Dolar</option>
-                        <option value="USDT">Dolar Turismo</option>
-                        <option value="CAD">Dolar Canadense</option>
-                        <option value="EUR">Euro</option>
-                        <option value="GBP">Libra Esterlina</option>
-                        <option value="ARS">Peso Argentino</option>
-                        <option value="BTC">Bitcoin</option>
-                        <option value="LTC">Litecoin</option>
-                        <option value="JPY">Yene Japonês</option>
-                        <option value="CHF">Franco Suiço</option>
-                        <option value="AUD">Dólar Australiano</option>
-                        <option value="CNY">Yuan Chinês</option>
-                        <option value="ILS">Novo Shekel Israelense</option>
-                        <option value="ETH">Ethereum</option>
-                        <option value="XRP">Ripple</option>
-                    </select>
+                    <label className="fonteform"> Qual moeda gostaria de converter do real?</label>
+                    
                     <br></br>
-                    <label className="fonteform"> Qual Moeda você deseja:</label>
-                    <select onChange={event => setMoeda(event.target.value)} value={moeda}>
+
+                    <select onChange={event => setMoeda(event.target.value)} value={moeda} className="valorcss">
                         <option value="BRL">Real</option>
                         <option value="USD">Dolar</option>
                         <option value="USDT">Dolar Turismo</option>
@@ -98,9 +86,11 @@ setValorconvert((valor/codigo[0].high).toFixed(2))
                     </select>
                     <br></br>
 
-                    <label className="fonteform">Qual a quantidade que deseja converter</label> 
-                    <input type="number" className="valorcss" 
-                    onChange={event => setValor(event.target.value)} value={valor}>
+                    <label className="fonteform">Qual a quantidade que deseja converter</label>
+                    <input type="number" className="valorcss"
+                        onChange={event => setValor(event.target.value)} value={valor}
+                        placeholder="Digite seu valor"
+                        step="0.2">
                     </input>
                     <br></br>
 
@@ -109,10 +99,12 @@ setValorconvert((valor/codigo[0].high).toFixed(2))
                         obterMoeda(moeda)
                     }}>Converter</button>
                     <br></br>
+
                     <label className="fonteform"> Sua moeda convertida foi:</label>
                     <br></br>
-                    <input type="text" className="valorcss" value={valorconvert}>
+                    <input type="text" className="resultadocss" value={valorconvert}>
                     </input>
+
                 </form>
             </Card>
         </>
